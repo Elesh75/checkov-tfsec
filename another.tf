0c89270cd4f88c5abe68d69c46c452ca1d66b8a1
elesh75 <oguntademujaheed2@gmail.com>
2023-07-08 12:26:14 +0100

    1356789

--- another.tf
+++ another.tf
@@ -73,7 +73,8 @@ resource "aws_s3_bucket_public_access_block" "my_bucket" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
+ }
+} 
 
 resource "aws_s3_bucket_versioning" "my_bucket" {
   bucket = aws_s3_bucket.my_bucket.id
